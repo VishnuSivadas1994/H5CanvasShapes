44cee6685dd2a9c165fe319924990f80b45d99b3
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-02-25 19:46:18 -0500

    basic circular movement using sine and cosine value

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 ///
-let size = 0;
+//let size = 0;
+let positionX = 100;
+let positionY = 100;
+let angle = 0;
 
 function circleLogic() {
-    size++;
+
     ctx.fillStyle = 'red';
     ctx.strokeStyle = 'blue';
     ctx.lineWidth = 5;
     ctx.beginPath();
-    ctx.arc(200, 300, size, 0, Math.PI * 2);
+    ctx.arc(positionX, positionY, 20, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
@@ -21,7 +24,13 @@ function circleLogic() {
 ///
 
 function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //size += 0.05;
+    positionX += 5 * Math.sin(angle);//-1 to 1 sin function.
+    positionY += 5 * Math.cos(angle);
+    angle += 0.1;
     circleLogic();
+    console.log(positionX + "----" + positionY);
 
     requestAnimationFrame(animate);
 }

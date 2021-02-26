cdee1ff2b173a37ad8ef2edbf0661a2917ec4b39
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-02-25 20:14:15 -0500

    animated flower

--- script.js
+++ script.js
@@ -3,19 +3,35 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-///
-//let size = 0;
-let positionX = 100;
-let positionY = 100;
-let angle = 0;
+// window.addEventListener('resize', function () {
+//     //Fixing the canvas size
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
+// });
 
-function circleLogic() {
+///GLOBAL
+let number = 0;
+let scale = 10;
 
+function flowerLogic() {
+    let angle = number * 7;
+    let radius = scale * Math.sqrt(number);
+
+    let positionX = radius * Math.sin(angle) + canvas.width / 2;
+    let positionY = radius * Math.cos(angle) + canvas.height / 2;
+
+    circleObject(positionX, positionY);
+
+
+    number++;
+}
+
+function circleObject(x, y) {
     ctx.fillStyle = 'red';
     ctx.strokeStyle = 'blue';
     ctx.lineWidth = 5;
     ctx.beginPath();
-    ctx.arc(positionX, positionY, 20, 0, Math.PI * 2);
+    ctx.arc(x, y, 20, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
@@ -24,13 +40,9 @@ function circleLogic() {
 ///
 
 function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //size += 0.05;
-    positionX += 5 * Math.sin(angle);//-1 to 1 sin function.
-    positionY += 5 * Math.cos(angle);
-    angle += 0.1;
-    circleLogic();
-    console.log(positionX + "----" + positionY);
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    flowerLogic();
+    if (number > 300) return;
 
     requestAnimationFrame(animate);
 }

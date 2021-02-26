1b2b4fb4ef8cee9019eb9b04f1f859f7a71e6086
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-02-25 20:24:57 -0500

    added variable hue

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.globalCompositeOperation = 'destination-over';
+let hue = Math.random() * 360;
 
 // window.addEventListener('resize', function () {
 //     //Fixing the canvas size
@@ -14,7 +16,7 @@ let number = 0;
 let scale = 10;
 
 function flowerLogic() {
-    let angle = number * 7;
+    let angle = number * 8.2;
     let radius = scale * Math.sqrt(number);
 
     let positionX = radius * Math.sin(angle) + canvas.width / 2;
@@ -24,10 +26,11 @@ function flowerLogic() {
 
 
     number++;
+    hue += 0.5;
 }
 
 function circleObject(x, y) {
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
     ctx.strokeStyle = 'blue';
     ctx.lineWidth = 5;
     ctx.beginPath();
@@ -42,7 +45,7 @@ function circleObject(x, y) {
 function animate() {
     // ctx.clearRect(0, 0, canvas.width, canvas.height);
     flowerLogic();
-    if (number > 300) return;
+    if (number > 1000) return;
 
     requestAnimationFrame(animate);
 }

34a6f1e8a5769a3521985b26c23ad3ad4688a47a
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-02-25 19:20:47 -0500

    basic animation framework

--- script.js
+++ script.js
@@ -4,12 +4,26 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 ///
+let size = 0;
 
-ctx.fillStyle = 'red';
-ctx.strokeStyle = 'blue';
-ctx.lineWidth = 5;
-ctx.beginPath();
-ctx.arc(200, 300, 50, 0, Math.PI * 2);
-ctx.closePath();
-ctx.fill();
-ctx.stroke();
+function circleLogic() {
+    size++;
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.arc(200, 300, size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+}
+
+///
+
+function animate() {
+    circleLogic();
+
+    requestAnimationFrame(animate);
+}
+
+animate();
